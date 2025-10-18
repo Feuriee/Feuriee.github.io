b6326120e95c6cf63f06eebace4ac10157e1df52
Feuriee <desi.gc123@gmail.com>
2025-10-18 22:22:39 +0700

    commit

---
+++ project.js
@@ -0,0 +1,33 @@
+// projects.js
+const projects = [
+  {
+    title: "Tugas 1 Pemrograman Web",
+    description: "Tugas 1 Pemrograman Web yang berisi tentang pembuatan halaman web sederhana menggunakan HTML dan CSS.",
+    link: "https://feuriee.github.io/Tugas-1-Praktikum-Pemrograman-Web-/"
+  },
+  {
+    title: "Tugas 2 Pemrograman Web",
+    description: "Tugas 2 Pemrograman Web yang berisi tentang pembuatan halaman web responsif menggunakan HTML, CSS, dan JavaScript.",
+    link: "https://feuriee.github.io/Tugas-2-Praktikum-Pemrograman-Web/"
+  },
+  {
+    title: "Tugas 3 Pemrograman Web",
+    description: "Tugas 3 Pemrograman Web yang berisi tentang pembuatan halaman web To Do List menggunakan HTML, CSS, dan JavaScript.",
+    link: "https://feuriee.github.io/Tugas-3-Praktikum-Pemrograman-Web/"
+  },
+  {
+    title: "TrainKu App",
+    description: "Aplikasi booking tiket kereta berbasis React Native. Jika ingin melihat project ini silahkan login menggunakan akun berikut: email: admin dan password: TrainKu ; dan akun satu lagi email: user dan password: user123",
+    link: "https://trainku--b3nua4mqav.expo.app/"
+  },
+  {
+    title: "FindYourAnime App",
+    description: "Aplikasi pencarian anime berbasis React Native.",
+    link: "https://findyouranime--i87punoi21.expo.app/"
+  },
+  {
+    title: "Game TikTakToe",
+    description: "Permainan TikTakToe sederhana menggunakan Java.",
+    link: "https://github.com/Feuriee/TikTakToe"
+  }
+];
